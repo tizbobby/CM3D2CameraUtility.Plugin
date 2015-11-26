239b5c8b793ede8696f8a4870d41b7703c80ea28
Milly <milly.ca@gmail.com>
2015-11-27 08:20:51 +0900

    fix env using.

--- compile.bat
+++ compile.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 pushd "%~pd0"
 
 if "%CM3D2_PLATFORM%" == "" set CM3D2_PLATFORM=x64
@@ -6,7 +7,6 @@ if "%CM3D2_MOD_DIR%" == ""  set CM3D2_MOD_DIR=C:\Games\KISS\CM3D2_MOD
 set REIPATCHER_DIR=%CM3D2_MOD_DIR%\ReiPatcher
 set UNITY_INJECTOR_DIR=%CM3D2_MOD_DIR%\UnityInjector
 set CM3D2_MOD_MANAGED_DIR=%CM3D2_MOD_DIR%\CM3D2%CM3D2_PLATFORM%_Data\Managed
-if "%OUTDIR%" == "" set OUTDIR=%UNITY_INJECTOR_DIR%
 
 
 set "CSC_REG_KEY=HKLM\SoftWare\Microsoft\NET Framework Setup\NDP\v3.5"
@@ -20,14 +20,17 @@ set "CSC=%CSC_PATH%\csc.exe"
 set TYPE=/t:library
 set TARGET=CM3D2CameraUtility.Plugin.dll
 set SRCS=^
+  CM3D2CameraUtility.Plugin\Properties\AssemblyInfo.cs ^
   CM3D2CameraUtility.Plugin\CM3D2CameraUtility.Plugin.cs
 set CSOPT=/optimize+
 set CSLIB=^
   /lib:"%REIPATCHER_DIR%" /r:ReiPatcher.exe /r:mono.cecil.dll /r:mono.cecil.rocks.dll ^
   /lib:"%UNITY_INJECTOR_DIR%" /r:UnityInjector.dll ^
   /lib:"%CM3D2_MOD_MANAGED_DIR%" /r:UnityEngine.dll /r:Assembly-CSharp.dll /r:Assembly-CSharp-firstpass.dll
-
+set OUTDIR=%UNITY_INJECTOR_DIR%
 
 %CSC% %CSOPT% %TYPE% %CSLIB% /out:%OUTDIR%\%TARGET% %SRCS% || exit /b 1
 
+
 popd
+endlocal
